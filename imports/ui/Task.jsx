5a50ef4f4f6c61d4a77730bc7758f94266f346e0
LeLwy <Louis.Chancioux@gmail.com>
2024-03-26 14:13:30 +0100

    Ajout d'un bouton supprimer sur les tasks et maj App.jsx

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -9,7 +9,7 @@ function formatDate() {
 	return dateNum + "/" + monthNum + "/" + yearNum;
 }
 
-export const Task = ({ task, onCheckboxClick }) => {
+export const Task = ({ task, onCheckboxClick, onDeleteClick }) => {
     return <li>
             <input 
                 type="checkbox" 
@@ -17,6 +17,8 @@ export const Task = ({ task, onCheckboxClick }) => {
                 onClick={()=> onCheckboxClick(task)} 
                 readOnly
             />
-            <span>{task.text}</span><span>, created at {formatDate(task.createdAt)}</span>
+            <span>{task.text}</span>
+            <span>, created at {formatDate(task.createdAt)}</span>
+            <button onClick={ () => onDeleteClick(task) }>&times;</button>
         </li>
 };
