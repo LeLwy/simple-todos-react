ddc4e5f8205d37d1f5b26b0f7b312e05d4eef82f
LeLwy <Louis.Chancioux@gmail.com>
2024-03-26 13:49:46 +0100

    Création du formulaire d'ajout de tâche et MAJ sur App et Task

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+function formatDate() {
+	const today = new Date();
+	// Month starts at 0, annoyingly
+	const monthNum = today.getMonth() < 10 ? "0" + (today.getMonth() + 1) : (today.getMonth() + 1);
+	const dateNum = today.getDate() < 10 ? "0" + today.getDate() : today.getDate();
+	const yearNum = today.getFullYear();
+	return dateNum + "/" + monthNum + "/" + yearNum;
+}
+
 export const Task = ({ task }) => {
-    return <li>{task.text}</li>
+    return <li>{task.text}, created at {formatDate(task.createdAt)}</li>
 };
