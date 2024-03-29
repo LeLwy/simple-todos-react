00d9e9d3eb7bea195c822e78fa37b1e95e42a699
LeLwy <Louis.Chancioux@gmail.com>
2024-03-29 19:30:15 +0100

    Création d'un utilisateur de base au lancement

--- server/main.js
+++ server/main.js
@@ -1,9 +1,20 @@
 import { Meteor } from 'meteor/meteor';
+import { Account } from 'meteor/accounts-base';
 import { TasksCollection } from '/imports/api/TasksCollection';
 
 const insertTask = taskText => TasksCollection.insert({ text: taskText });
 
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
+
 Meteor.startup(() => {
+  if(!Account.findUserByUsername(SEED_USERNAME)) {
+    Account.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  };
+
   if (TasksCollection.find().count() === 0) {
     [
       'First Task',
@@ -14,5 +25,5 @@ Meteor.startup(() => {
       'Sixth Task',
       'Seventh Task'
     ].forEach(insertTask)
-  }
+  };
 });
